ed95bff80258f40301220b60fa28fa8ce3ddb2f7
RAVI SHARMA <ravics09@RAVIs-MacBook-Air.local>
2021-07-21 02:05:59 +0530

    Implemented Setting Screen

--- src/screens/settingScreen.js
+++ src/screens/settingScreen.js
@@ -1,11 +1,50 @@
-import React from 'react'
-import { View, Text } from 'react-native';
+import React from 'react';
+import {View, Text, Share, StyleSheet, TouchableOpacity, Linking} from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const SettingScreen = () => {
-    return(
-        <View>
-            <Text>Setting Screen</Text>
-        </View>
-    )
-}
+  return (
+    <View style={{flex: 1, backgroundColor: '#F0F8FF', paddingTop: 10}}>
+      <View style={{alignItems: 'center'}}>
+        <Text style={{color: '#FF4500', fontSize: 20, fontWeight: 'bold'}}>
+          Setting
+        </Text>
+      </View>
+
+      <View style={{flex: 1, MarginLeft: 10}}>
+        <TouchableOpacity
+          style={styles.item}
+          onPress={() =>
+            Share.share({
+              message: 'Microwave Recipe Handbook',
+            })
+          }>
+          <Ionicons name="share-social-sharp" color="black" size={23} />
+          <Text style={{paddingLeft: 10, fontSize: 20}}>Share App</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.item}>
+          <Ionicons name="star-half-sharp" color="black" size={23} />
+          <Text style={{paddingLeft: 10, fontSize: 20}}>Rate Our App</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.item} onPress={()=>Linking.openURL("http://play.google.com/store/apps/details?id=in.bansalindia.microwaveovenrecipeshindi")}>
+          <Ionicons name="apps-sharp" color="black" size={23} />
+          <Text style={{paddingLeft: 10, fontSize: 20}}>
+            Many More Application
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  item: {
+    paddingLeft: 10,
+    paddingTop: 10,
+    fontSize: 23,
+    flexDirection: 'row',
+  },
+});
 export default SettingScreen;
