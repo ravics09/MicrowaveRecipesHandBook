99771abd50f48289ca192dbec1b139502ee1f498
RAVI SHARMA <ravics09@RAVIs-MacBook-Air.local>
2021-07-20 16:57:32 +0530

    Added splash screen

--- App.js
+++ App.js
@@ -1,7 +1,12 @@
-import React, {PureComponent} from 'react';
+import React, {useEffect} from 'react';
 import {View, Text, SafeAreaView, StatusBar} from 'react-native';
+import RNBootSplash from "react-native-bootsplash";
 
 const App = () => {
+  useEffect(() => {
+    RNBootSplash.hide({ duration: 250 });
+  }, []);
+
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#6a51ae" />
---
+++ src/screens/index.js
@@ -0,0 +1,15 @@
+import HomeScreen from './homeScreen';
+import CategoriesScreen from './categoriesScreen';
+import CategoryScreen from './categoryScreen';
+import FavouriteScreen from './favouriteScreen';
+import RecipeScreen from './recipeScreen';
+import SettingScreen from './settingScreen';
+
+export {
+    HomeScreen,
+    CategoriesScreen,
+    CategoryScreen,
+    FavouriteScreen,
+    RecipeScreen,
+    SettingScreen
+}
